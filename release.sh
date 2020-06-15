55bd6f40258f5734becc64388c9ed3a2049f1b87
Van Cliff Le <cliff.le.van@consol.de>
2020-06-15 10:47:12 +0200

    Apply suggestions from code review
    Co-authored-by: Sven Hettwer <sven.hettwer@web.de>

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ function help() {
     echo "Usage: sh release.sh <RELEASE_VERSION>"
     echo ""
     echo "Parameters:"
-    echo "  RELEASE_VERSION: SemVer Version number for the release"
+    echo "  RELEASE_VERSION: SemVer Version of the release"
 }
 
 semver_pattern="^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$"
@@ -14,8 +14,9 @@ RELEASE_VERSION=${1}
 [[ -z "${RELEASE_VERSION}" ]] && echo "ERROR: RELEASE_VERSION is empty" && help  && exit 1
 [[ ! "${RELEASE_VERSION}" =~ $semver_pattern ]] && echo "ERROR: RELEASE_VERSION does not match the SemVer specification" && help && exit 1
 
+git fetch
 printf "\n%s\n" "Create new branch release/${RELEASE_VERSION}"
-git checkout -b release/${RELEASE_VERSION}
+git checkout -b release/${RELEASE_VERSION} origin/develop
 
 printf "\n%s\n"  "Change Version in package.json"
 npm version ${RELEASE_VERSION} --no-git-tag-version
